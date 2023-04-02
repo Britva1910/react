fb078679c34ba1482d85e28fc262cadd8d96fb87
Ivan Lashuk <85491523+Britva1910@users.noreply.github.com>
2023-04-02 18:24:51 +0300

    feat: add modal page

--- RSS-React/src/components/pages/form-page/FormPage.tsx
+++ RSS-React/src/components/pages/form-page/FormPage.tsx
@@ -1,43 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from '../../form/Form';
 import PersonCard from '../../personCard/PersonCard';
 import styles from './form-page.module.scss';
 import { IPersonCard } from '../../../shared/models';
+import ModalPage from '../../modalPage/ModalPage';
 
-type FormPageState = {
-  cards: IPersonCard[];
-};
+const FormPage: React.FC = () => {
+  const [cards, setCards] = useState<IPersonCard[]>([]);
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
 
-export default class FormPage extends React.Component<Record<string, never>, FormPageState> {
-  constructor(props: Record<string, never>) {
-    super(props);
-    this.state = { cards: [] };
-    this.updateStateFormPage = this.updateStateFormPage.bind(this);
-  }
-
-  updateStateFormPage(value: IPersonCard) {
+  const updateStateFormPage = (value: IPersonCard) => {
     const file = value.file?.[0];
     if (file) {
       const blob = new Blob([file], { type: 'application/pdf' });
       value.file = URL.createObjectURL(blob);
     }
-    this.setState((prevState) => ({
-      cards: [...prevState.cards, value],
-    }));
-  }
+    setCards([...cards, value]);
+    setModalVisible(true);
+  };
 
-  render() {
-    return (
-      <div>
-        <Form updateData={this.updateStateFormPage} />
-        <ul className={styles.card_list}>
-          {this.state.cards.map((item, index) => (
-            <li key={index}>
-              <PersonCard data={item} />
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+  const closeModalPage = () => {
+    setModalVisible(false);
+  };
+
+  return (
+    <div>
+      <Form updateData={updateStateFormPage} />
+      <ul className={styles.card_list}>
+        {cards.map((item, index) => (
+          <li key={index}>
+            <PersonCard data={item} />
+          </li>
+        ))}
+      </ul>
+      <ModalPage modalVisible={modalVisible} closePage={closeModalPage} />
+    </div>
+  );
+};
+
+export default FormPage;
